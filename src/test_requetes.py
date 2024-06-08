67f89f2eaadb2578a47e40e5f5ca6ce7db3300c9
ugodominguez <ugo.dominguez@etu.univ-orleans.fr>
2024-06-08 17:48:34 +0200

    add test for collaborateurs_communs

--- src/test_requetes.py
+++ src/test_requetes.py
@@ -18,7 +18,16 @@ class test_case(unittest.TestCase):
         self.assertEqual(graphe.adj, self.G.adj)
 
     def test_collaborateurs_communs(self):
-        ...
+        res = r.collaborateurs_communs(self.G, "a1", "d1")
+        self.assertEqual(res, {'a3', 'd3', 'p2', 'd2', 'p1', 'p3', 'a2'})
+        res = r.collaborateurs_communs(self.G, "p5", "d3")
+        self.assertEqual(res, set())
+        res = r.collaborateurs_communs(self.G, "a4", "a3")
+        self.assertEqual(res, {"a2", "a5", "d2", "p2"})
+        res = r.collaborateurs_communs(self.G, "a2", "p4")
+        self.assertEqual(res, {"p2", "a4", "a6", "d4", "d6", "p6", "d2"})
+        res = r.collaborateurs_communs(self.G, "z5", "z6")
+        self.assertIsNone(res)
 
     def test_collaborateurs_proches(self):
         ...
