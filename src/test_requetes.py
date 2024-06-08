b59a8b869b4ba5a19fadb0e126727f799c1a02c5
ugodominguez <ugo.dominguez@etu.univ-orleans.fr>
2024-06-08 17:47:36 +0200

    add const file and test for json_vers_nx

--- src/test_requetes.py
+++ src/test_requetes.py
@@ -3,6 +3,7 @@ import unittest
 import networkx as nx
 
 import requetes as r
+import const as c
 
 
 """
@@ -10,10 +11,11 @@ Classe de test des fonctions du fichier requetes.py
 """
 class test_case(unittest.TestCase):  
     def setUp(self):
-        ...
+        self.G = nx.Graph(c.DICO_TEST)
 
     def test_json_vers_nx(self):
-        ...
+        graphe = r.json_vers_nx(c.FIC_PATH / "data_test.txt")
+        self.assertEqual(graphe.adj, self.G.adj)
 
     def test_collaborateurs_communs(self):
         ...
